f3a603553aad53db4499953dd68c99e7a66530f1
Markus Heldrup <markusheldrup@hotmail.com>
2018-09-03 13:19:50 +0200

    Upadted lots of stuff

--- public/javascripts/combat.js
+++ public/javascripts/combat.js
@@ -1,33 +1,30 @@
+//Combat Crime
+console.log("Combat-stuff")
+
+
 //Combat log
-console.log("jiji")
-// function combatLog(array, id, opponent) {
-//     //console.log(array)
-//     if (array.length !== 1) {
-//         if (array[array.length-1] == "1") {
-//            document.getElementById("writing").innerHTML += array.shift();
-//            setTimeout(function() {
-//                combatLog(array, id, opponent)
-//            }, 8)
-//         }
-//         if (array[array.length-1] == "4") {
-//             document.getElementById("writing").innerHTML += array.shift()
-//             setTimeout(function() {
-//                 combatLog(array, id, opponent)
-//             }, 30)
-//         }
-//         if (array[array.length-1] == "5") {
-//           document.getElementById("writing").innerHTML += "<br><br><br>" + array.slice(0, array.length-1);
-//         return;
-//          }
-//     } else {
-//         document.getElementById("loading-bar").style.width = (100 - opponent.hp/opponent.maxHp * 100) + "%";
-//         console.log("hackhackhack")
-//         return id.fight(opponent)
-//     }   
-// }
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    console.log('IronGenerator JS imported successfully!');
-  
-  }, false);
+function combatLog(array, id, opponent) {
+    if (array.length !== 1) {
+        if (array[array.length-1] == "1") {
+           document.getElementById("writing").innerHTML += array.shift();
+           setTimeout(() => {
+               combatLog(array, id, opponent)
+           }, 8)
+        }
+        if (array[array.length-1] == "4") {
+            document.getElementById("writing").innerHTML += array.shift()
+            setTimeout(() => {
+                combatLog(array, id, opponent)
+            }, 30)
+        }
+        if (array[array.length-1] == "5") {
+          document.getElementById("writing").innerHTML += "<br><br><br>" + array.slice(0, array.length-1);
+        return;
+         }
+    } else {
+        document.getElementById("loading-bar").style.width = (100 - opponent.hp/opponent.maxHp * 100) + "%";
+        console.log("hackhackhack")
+        return id.fight(opponent)
+    }   
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET all routes. */
 router.get('/index', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });

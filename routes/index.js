836ec47bbff0d08b612a5fa0b13be3a6c8b49c8f
Markus Heldrup <markusheldrup@hotmail.com>
2018-09-05 12:24:09 +0200

    PvP fixed

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models/User");
-const Crime = require("../models/Crime")
-const Alliance = require("../models/Alliance")
-const Item = require("../models/Item")
-const uploadCloud = require('../utils/cloudinary.js');
+const Crime = require("../models/Crime");
+const Alliance = require("../models/Alliance");
+const Item = require("../models/Item");
+const uploadCloud = require("../utils/cloudinary.js");
 
 /* GET all routes. */
 router.get("/index", (req, res, next) => {
   res.render("index", { title: "Express" });
 });
-// npm install ensureLogin see 
+// npm install ensureLogin see
 //atm ensureAuthenticated is not doing anything
-router.get("/create-hacker", ensureAuthenticated, (req, res, next) => { // ensure user is logged in
+router.get("/create-hacker", ensureAuthenticated, (req, res, next) => {
+  // ensure user is logged in
   res.render("create-hacker", { title: "Express" });
 });
 
@@ -20,7 +21,7 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
-    res.redirect('/sign-in')
+    res.redirect("/sign-in");
   }
 }
 
@@ -29,22 +30,23 @@ router.post("/create-hacker", uploadCloud.single("photo"), (req, res, next) => {
   const imgPath = req.file.url;
   const imgName = req.file.originalname;
 
-  console.log('req user consolelog',req.user)
+  console.log("req user consolelog", req.user);
   //User.findById /// add { title, description, imgPath, imgName });
-  User.findByIdAndUpdate(req.user._id, { title, description, imgPath, imgName }).then((result) => {
-    console.log(result);
-    res.render('index')
-  })
+  User.findByIdAndUpdate(req.user._id, { title, description, imgPath, imgName })
+    .then(result => {
+      console.log(result);
+      res.render("index");
+    })
 
-  // const newUser = new User({ title, description, imgPath, imgName });
-  // newUser
-  //   .save()
-  //   .then(user => {
-  //     res.redirect("/");
-  //   })
+    // const newUser = new User({ title, description, imgPath, imgName });
+    // newUser
+    //   .save()
+    //   .then(user => {
+    //     res.redirect("/");
+    //   })
     .catch(error => {
       console.log(error);
-      res.redirect('error');
+      res.redirect("error");
     });
 });
 
@@ -62,49 +64,33 @@ router.get("/hack/crimes", (req, res, next) => {
 });
 
 router.get("/hack/crimes/:id", (req, res, next) => {
-  let userIdThing = User.findById(req.user._id)
-  let crimeIdThing = Crime.findById(req.params.id)
+  let userIdThing = User.findById(req.user._id);
+  let crimeIdThing = Crime.findById(req.params.id);
 
-  Promise.all([userIdThing, crimeIdThing]).then((result) => {
-    let player = result[0];
-    let crimeToCommit = result[1];
-    let resultCrime = player.fightCrime(crimeToCommit);
-    console.log(resultCrime); 
-  })
+  Promise.all([userIdThing, crimeIdThing]).then(result => {
+    let resultCrime = result[0].fightCrime(result[1]);
+    if (!resultCrime) return res.send("Insufficient battery, or firewall, or the enemy is already dead!");
+    res.render("menu/hack-crimes-id", {
+      result: JSON.stringify(resultCrime)
+    });
+  });
 });
 
-  // Crime.findById(req.params.id).then(result => {
-  //   crimeToCommit = result;
-  //   res.render("menu/hack-crimes-id", {
-  //     result: JSON.stringify(crimeToCommit)
-  //   });
-  // });
-
-// TODO get the user from req.user
-// get the crime from db
-// do the actual fight: let result = fight()
-// render the result page with the result
-
-//   res.render("menu/hack-crimes", {
-//     result: JSON.stringify({ rounds: [{dodged: true}], won: true})
-//   })
-// })
-
 router.get("/hack/hack-player", (req, res, next) => {
-  User.find({}).then((user) => {
-    res.render("menu/hack-player", {user});
-  })
+  User.find({}).then(user => {
+    res.render("menu/hack-player", { user });
+  });
 });
 
 router.get("/hack/hack-player/:id", (req, res, next) => {
-  let newReq = req.params.id.slice(1)
-  let userIdThing = User.findById(req.user._id)
-  let opponentIdThing = User.findById(newReq)
-
-  Promise.all([userIdThing, opponentIdThing]).then((result) => {
-   // let resultHack = result[0].hackPlayer(result[1]);
-    res.render("menu/hack-player-id", {result: JSON.stringify(resultHack)})
-  })
+  let newReq = req.params.id.slice(1);
+  let userIdThing = User.findById(req.user._id);
+  let opponentIdThing = User.findById(newReq);
+  Promise.all([userIdThing, opponentIdThing]).then(result => {
+    let resultHack = result[0].hackPlayer(result[1]);
+    if (!resultHack) return res.send("Insufficient battery");
+    res.render("menu/hack-player-id", { result: JSON.stringify(resultHack) });
+  });
 });
 
 router.get("/hack/wanted-list", (req, res, next) => {
@@ -127,21 +113,20 @@ router.get("/alliance/hideout", (req, res, next) => {
   res.render("menu/alliance-hideout");
 });
 
-router.get("/marketplace", (req,res, next) => {
+router.get("/marketplace", (req, res, next) => {
   Item.find()
-  .then(items => {
-    res.render("menu/marketplace", { 
-      items,
-      cpuItems: items.filter(i => i.type === "cpu"),
-      firewallItems: items.filter(i => i.type === "firewall"),
-      avsItems: items.filter(i => i.type === "avs"),
-      encryptionItems: items.filter(i => i.type === "encryption"),
+    .then(items => {
+      res.render("menu/marketplace", {
+        items,
+        cpuItems: items.filter(i => i.type === "cpu"),
+        firewallItems: items.filter(i => i.type === "firewall"),
+        avsItems: items.filter(i => i.type === "avs"),
+        encryptionItems: items.filter(i => i.type === "encryption")
+      });
+    })
+    .catch(error => {
+      console.log(error);
     });
-   
-  })
-  .catch(error => {
-    console.log(error)
-  })
 });
 router.get("/system-repair", (req, res, next) => {
   res.render("menu/system-repair");
@@ -168,8 +153,27 @@ router.get("/logout", (req, res, next) => {
 });
 
 router.post("/upload", (req, res) => {
-  console.log(req.files)
-  res.send("we got the file")
-})
+  console.log(req.files);
+  res.send("we got the file");
+});
 
 module.exports = router;
+
+//NOTES:
+
+// Crime.findById(req.params.id).then(result => {
+//   crimeToCommit = result;
+// res.render("menu/hack-crimes-id", {
+//   result: JSON.stringify(crimeToCommit)
+// });
+// });
+
+// TODO get the user from req.user
+// get the crime from db
+// do the actual fight: let result = fight()
+// render the result page with the result
+
+//   res.render("menu/hack-crimes", {
+//     result: JSON.stringify({ rounds: [{dodged: true}], won: true})
+//   })
+// })
